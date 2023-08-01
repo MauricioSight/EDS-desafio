e0b8c69404bfeba0fdb7e141596e5b811e280fff
MauricioSight <mauriciosightman@gmail.com>
2023-08-01 13:37:31 -0300

    refactor: setup problem 6

--- setup/problema6_create_json.py
+++ setup/problema6_create_json.py
@@ -1,16 +1,28 @@
 import csv
 import json
 
-# Cria json file a partir do arquivo gerado pelo SIGTAP
-def create_json_file(layout_path, data_path, json_path):
-        with open(layout_path, 'r') as layout_file, open(data_path, 'r', encoding="ISO-8859-1") as data_file, open(json_path, 'w', encoding="ISO-8859-1") as json_file:
+def main():
+    create_json_file("../data/tb_procedimento_layout.txt", "../data/tb_procedimento.txt", "../data/tb_procedimento.json")
+ 
 
+def create_json_file(layout_path, data_path, json_path):
+        """
+        Cria json file a partir do arquivo gerado pelo SIGTAP
+        Arguments:
+            layout_path: Path do arquivo de layout
+            data_path: Path do arquivo dos dados
+            json_path: Path do arquivo json escrito
+        """
+        with ( 
+            open(layout_path, 'r') as layout_file, open(data_path, 'r', encoding="ISO-8859-1") as data_file, 
+            open(json_path, 'w', encoding="ISO-8859-1") as json_file
+        ):
             layout = csv.DictReader(layout_file)
             data = data_file.readlines()
 
             json_dic = {"layout": [], "data": []}
 
-            # Pega as colunas e range referente ao valor no arquivo
+            # Pega as colunas e intervalo do index referente ao valor no arquivo
             layout_columns = []
             for column in layout:
                 json_dic["layout"].append(column)
@@ -23,18 +35,24 @@ def create_json_file(layout_path, data_path, json_path):
                     "index_range": (start_index, end_index) 
                 })
 
-            # Para cada linha do arquivo, adiciona no banco
+            # Adiciona em 'json_dic' cada valor do arquivo csv
             for row in data:
                 row_value = {}
+
+                # Relação linha, coluna
                 for column in layout_columns:
                     start_index, end_index = column["index_range"]
                     value = row[start_index:end_index].strip()
+
+                    # Lida com valores vazios
                     if len(value) > 0:
                         row_value[column["name"]] = value
                     else:
                         row_value[column["name"]] = None
+
                 json_dic["data"].append(row_value)         
 
             json.dump(json_dic, json_file)
 
-create_json_file("../data/tb_procedimento_layout.txt", "../data/tb_procedimento.txt", "../data/tb_procedimento.json")
+
+main()
